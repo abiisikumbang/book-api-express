54032c995646d538458f5b88427ad8f8c834a6cd
abiisikumbang <128027741+abiisikumbang@users.noreply.github.com>
2025-09-05 22:03:59 +0700

    fix: error handler dengan stack dan production

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,15 @@
 function errorHandler(err, req, res, next) {
-  console.error(err);
-  res.status(err.status || 500).json({
+  console.error(err.stack);
+
+  const statusCode = err.status || 500;
+  const message =
+    process.env.NODE_ENV === "production" && statusCode === 500
+      ? "Internal Server Error"
+      : err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
     error: true,
-    message: err.message || "Internal Server Error",
+    message: message,
   });
 }
 
